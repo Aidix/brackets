8079efd2eb10f825d32050f91fbb08ed8d3d014d
Zhenya <zheka1609@yandex.ru>
2019-03-22 00:18:49 +0300

    Create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var strr = str.split(''),
+      stack = [],
+      open = [],
+      close = [],
+      openI, closeI, openII;
+
+      for (var i=0; i < bracketsConfig.length; i++){
+        open.push(bracketsConfig[i][0]);
+        close.push(bracketsConfig[i][1]);
+      }
+
+      for(i=0; i < strr.length; i++){
+
+        openI = open.indexOf(strr[i]);
+        closeI = close.indexOf(strr[i])
+
+        if(openI !== -1){
+          openII = stack[stack.length-1];
+          if(openII !== closeI){
+            stack.push(openI);
+            continue;
+          }
+        }
+
+        if(closeI !== -1){
+          openI = stack.pop();
+          if(closeI !== openI){
+            return false;
+          }
+        }
+      }
+      
+      if(stack.length !== 0){
+        return false;
+      }
+      return true;
 }
+
+
+
